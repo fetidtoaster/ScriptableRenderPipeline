59ea1476e09c46fb0d37e2d4664da00f2126d785
Levi Bard <levi@unity3d.com>
2017-10-05 14:06:26 +0200

    Add test for scene view deferred draw mode blacklisting under SRP

---
+++ Tests/SceneViewTests/Editor/CameraDrawModes.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class CameraDrawModes
+{
+
+	[Test]
+	public void DeferredModesAreDisabled()
+	{
+	    SceneView sceneView = EditorWindow.GetWindow<SceneView>();
+	    Assert.IsFalse(sceneView.IsCameraDrawModeEnabled(SceneView.GetBuiltinCameraMode(DrawCameraMode.DeferredDiffuse)), "Deferred modes should always be disabled under SRP");
+	}
+}
